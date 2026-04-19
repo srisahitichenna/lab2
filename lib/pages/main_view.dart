6a8246bc529613196d7e830e836aed1d584b4fe6
srisahitichenna <srisahitichenna@gmail.com>
2026-04-19 20:02:10 +0200

    hii

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab2/widgets/difficulty_control.dart';
 import 'package:lab2/widgets/ingredient_control.dart';
 import 'package:lab2/widgets/kitchen_control.dart';
 
@@ -26,19 +27,27 @@ Widget _controlPanel(context, { double width = 320}) {
           child: Text( 
             'My Title')
           ),
-          Text( 'Hitta ett recept som passar genom att ändra iställningarna nedanför'),
+        Text( 'Hitta ett recept som passar genom att ändra iställningarna nedanför'),
 
-          Row(
-            children: [
-              Text(" Ingrediens:"),
-              SizedBox( width: 12),
-              IngredientControl()]),
+        Row(
+          children: [
+            Text(" Ingrediens:"),
+            SizedBox( width: 12),
+            IngredientControl()]),
 
-          Row(
+         Row(
+          children: [
+            Text(" Kök:"),
+            SizedBox( width: 12),
+            KitchenControl()]),
+
+        
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(" Kök:"),
-              SizedBox( width: 12),
-              KitchenControl()])
+              Text( "Svårighetsgrad"),
+              SizedBox( width: 10),
+              DifficulyControl()])
       ],
     )
     
--- lib/widgets/difficulty_control.dart
+++ lib/widgets/difficulty_control.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab2/util/difficulty.dart';
 
 class DifficulyControl extends StatefulWidget {
    const DifficulyControl({super.key});
@@ -8,8 +9,26 @@ class DifficulyControl extends StatefulWidget {
 }
 
 class _DifficulyControlState extends State<DifficulyControl> {
+  String _difficulty = Difficulty.labels[0];
    @override
-   Widget build(BuildContext context) {
-      return const Placeholder();
-   }
+  Widget build(BuildContext context) {
+    return RadioGroup<String>(
+      groupValue: _difficulty,
+      onChanged: (value) {
+        setState(() {
+          _difficulty = value!;
+        });
+      },
+      child: Column(
+        children: [
+          for (final label in Difficulty.labels)
+            RadioListTile<String>(
+              dense: true,
+              title: Text(label),
+              value: label,
+            ),
+        ],
+      ),
+    );
+  }
 }
